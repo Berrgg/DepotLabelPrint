dc8b6e66177b5e3b1808d42b4ff01e48661be5c4
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-04-22 11:20:44 +0100

    Add calendar control and label

--- DepotLabelPrint/View/MainView.Designer.cs
+++ DepotLabelPrint/View/MainView.Designer.cs
@@ -28,25 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dateEdit1 = new DevExpress.XtraEditors.DateEdit();
+            this.labelControl_DepotDate = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dateEdit1
+            // 
+            this.dateEdit1.EditValue = null;
+            this.dateEdit1.Location = new System.Drawing.Point(12, 58);
+            this.dateEdit1.Name = "dateEdit1";
+            this.dateEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.dateEdit1.Properties.Appearance.Options.UseFont = true;
+            this.dateEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit1.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit1.Properties.CalendarView = DevExpress.XtraEditors.Repository.CalendarView.Fluent;
+            this.dateEdit1.Properties.VistaDisplayMode = DevExpress.Utils.DefaultBoolean.False;
+            this.dateEdit1.Size = new System.Drawing.Size(123, 26);
+            this.dateEdit1.TabIndex = 0;
+            // 
+            // labelControl_DepotDate
+            // 
+            this.labelControl_DepotDate.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.labelControl_DepotDate.Appearance.Options.UseFont = true;
+            this.labelControl_DepotDate.Location = new System.Drawing.Point(13, 33);
+            this.labelControl_DepotDate.Name = "labelControl_DepotDate";
+            this.labelControl_DepotDate.Size = new System.Drawing.Size(83, 19);
+            this.labelControl_DepotDate.TabIndex = 1;
+            this.labelControl_DepotDate.Text = "Depot date:";
             // 
             // DepotLabelPrint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(694, 473);
+            this.Controls.Add(this.labelControl_DepotDate);
+            this.Controls.Add(this.dateEdit1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.LookAndFeel.SkinName = "Visual Studio 2013 Dark";
             this.LookAndFeel.UseDefaultLookAndFeel = false;
             this.Name = "DepotLabelPrint";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Depot Label Pint";
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private DevExpress.XtraEditors.DateEdit dateEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl_DepotDate;
     }
 }
 
