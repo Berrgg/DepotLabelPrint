42bede067c1bcbee2b79fcc766ac2e7118154e21
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-04-22 19:16:40 +0100

    Add a new class SqlQuery and function QueryDepotList

---
+++ DepotLabelPrint/DataAccess/SqlQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess.Sql;
+
+namespace DepotLabelPrint.DataAccess
+{
+    public class SqlQuery
+    {
+        CustomSqlQuery QueryDepotList()
+        {
+            CustomSqlQuery query = new CustomSqlQuery
+            {
+                Name = "queryDepotList",
+                Sql = "SELECT customername FROM si_sop_customers WHERE master_customer='msho' and active=-1 and customer_type='D' and payment_type=2"
+            };
+
+            return query;
+        }
+    }
+}
