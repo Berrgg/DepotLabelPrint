b9b7cbffbbd3ce015b62d74a440238f8fd9c2d07
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-04-22 15:35:28 +0100

    Add interface and SI database connection class

---
+++ DepotLabelPrint/DataAccess/ISqlDatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+
+
+namespace DepotLabelPrint.DataAccess
+{
+    public interface ISqlDatabaseConnection
+    {
+        MsSqlConnectionParameters DatabaseConnection { get; set; }
+
+    }
+}
