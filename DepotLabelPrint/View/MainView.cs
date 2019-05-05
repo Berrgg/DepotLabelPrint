e080e792c2d4fb7cfecc401f79203f5ddb058a74
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-05-05 13:04:09 +0100

    Code clean

--- DepotLabelPrint/View/MainView.cs
+++ DepotLabelPrint/View/MainView.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using DevExpress.DataAccess.ConnectionParameters;
-using DevExpress.DataAccess.Sql;
 using DepotLabelPrint.DataAccess;
 using DepotLabelPrint.Reports;
-using DevExpress.DataAccess.Native.Sql;
 using DevExpress.XtraReports.UI;
 
 namespace DepotLabelPrint
